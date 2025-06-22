a20cde1d5848752c1a837b6dc172bc3cd514e4e2
Alaq A.Ryanta <message.driyant@outlook.com>
2025-06-22 18:36:24 +0700

    feat: initial commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { GoogleGenerativeAI } = require("@google/generative-ai");
+const cors = require("cors");
+const port = 3000;
+
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(cors());
+app.use(express.static("public"));
+
+const genAI = new GoogleGenerativeAI(process.env.GOOGLE_API_KEY);
+const model = genAI.getGenerativeModel({ model: "models/gemini-2.5-flash" });
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Gemini AI App is running" });
+});
+
+app.post("/api/chat", async (req, res) => {
+  try {
+    const { prompt } = req.body;
+    if (!prompt) {
+      return res.status(400).json({ error: "Prompt is required" });
+    }
+    const result = await model.generateContent(prompt);
+    const resp = result.response;
+    res.status(200).json({ output: resp.text() });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ error: error });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Gemini AI App is running on port: ${port}`);
+});
